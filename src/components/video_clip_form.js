30444c7b7a0f7d78fec16207839751e988cda46b
gabalicious <gabalicious113@gmail.com>
2017-04-21 02:57:38 -0400

    Finish update video form and project

--- src/components/video_clip_form.js
+++ src/components/video_clip_form.js
@@ -3,12 +3,12 @@ import React from 'react';
 class VideoClipForm extends React.Component {
   constructor(props) {
     super(props);
-    const {title, duration, removable, onUpdateClipping, onCreateClipping} = props;
-    this.state = { title, start: duration[0], end: duration[1], removable};
+    const {title, duration, removable, onUpdateClipping, onCreateClipping, _id} = props;
+    this.state = { title, start: duration[0], end: duration[1], removable, _id};
     [this.onCreateClipping, this.onUpdateClipping] = [onCreateClipping, onUpdateClipping]
   }
-  componentWillReceiveProps({title, duration, removable}){
-    this.setState({title, start: duration[0], end: duration[1], removable});
+  componentWillReceiveProps({title, duration, removable, _id}){
+    this.setState({title, start: duration[0], end: duration[1], removable, _id});
   }
   render(){
     // update clip
@@ -28,35 +28,44 @@ class VideoClipForm extends React.Component {
             <div className="input-group-addon">End</div>
             <input type="text" onChange={(e)=>{this.setState({end: e.target.value})}} className="form-control" value={this.state.end} type="number" placeholder="Finish"/>
           </div>
-          <button type="submit" onClick={()=>{ this.onUpdateClipping({title: this.state.title, start: this.state.start, end: this.state.end})}} className="btn btn-success col-xs-3">Update Clipping</button>
-        </form>
-      );
-    }
-    // Create new clip
-    return (
-      <form className="form-inline video_clip_form row col-xs-12">
-        <div className="input-group mb-2 mr-sm-2 mb-sm-0 col-xs-5">
-          <div className="input-group-addon">Clip Name</div>
-          <input type="text" onChange={(e)=>{this.setState({title: e.target.value})}} value={this.state.title} className="form-control"  type="text"/>
-        </div>
-        <div className="input-group mb-2 mr-sm-2 mb-sm-0 col-xs-2">
-          <div className="input-group-addon">Start</div>
-          <input type="text" onChange={(e)=>{this.setState({start: e.target.value})}} className="form-control" value={this.state.start} min="0" max="52" type="number" placeholder="Start"/>
-        </div>
+          <button type="submit" onClick={(e)=>{
+            e.preventDefault();
+            this.onUpdateClipping({
+              title: this.state.title,
+              start: this.state.start,
+              end: this.state.end,
+              _id: this.state._id
+            })}} className="btn btn-success col-xs-3">Update Clipping</button>
+          </form>
+        );
+      }
+      // Create new clip
+      return (
+        <form className="form-inline video_clip_form row col-xs-12">
+          <div className="input-group mb-2 mr-sm-2 mb-sm-0 col-xs-5">
+            <div className="input-group-addon">Clip Name</div>
+            <input type="text" onChange={(e)=>{this.setState({title: e.target.value})}} value={this.state.title} className="form-control"  type="text"/>
+          </div>
+          <div className="input-group mb-2 mr-sm-2 mb-sm-0 col-xs-2">
+            <div className="input-group-addon">Start</div>
+            <input type="text" onChange={(e)=>{this.setState({start: e.target.value})}} className="form-control" value={this.state.start} min="0" max="52" type="number" placeholder="Start"/>
+          </div>
 
-        <div className="input-group mb-2 mr-sm-2 mb-sm-0 col-xs-2">
-          <div className="input-group-addon">End</div>
-          <input type="text" onChange={(e)=>{this.setState({end: e.target.value})}} className="form-control" value={this.state.end} min="0" max="52" type="number" placeholder="Finish"/>
-        </div>
-        <button type="submit" onClick={(e)=>{ this.onCreateClipping({
-          title: this.state.title,
-          duration: [this.state.start, this.state.end],
-          url: `http://grochtdreis.de/fuer-jsfiddle/video/sintel_trailer-480.mp4#t=${this.state.start},${this.state.end}`,
-          controls: false,
-          removable: true
-        })}}  className="btn btn-primary col-xs-3">Create Clipping</button>
-      </form>
-    );
-  };
-}
-export default VideoClipForm;
+          <div className="input-group mb-2 mr-sm-2 mb-sm-0 col-xs-2">
+            <div className="input-group-addon">End</div>
+            <input type="text" onChange={(e)=>{this.setState({end: e.target.value})}} className="form-control" value={this.state.end} min="0" max="52" type="number" placeholder="Finish"/>
+          </div>
+          <button type="submit" onClick={(e)=>{
+            e.preventDefault();
+            this.onCreateClipping({
+              title: this.state.title,
+              duration: [this.state.start, this.state.end],
+              url: `http://grochtdreis.de/fuer-jsfiddle/video/sintel_trailer-480.mp4#t=${this.state.start},${this.state.end}`,
+              controls: false,
+              removable: true
+            })}}  className="btn btn-primary col-xs-3">Create Clipping</button>
+          </form>
+        );
+      };
+    }
+    export default VideoClipForm;
--- src/index.js
+++ src/index.js
@@ -11,40 +11,16 @@ class App extends Component{
       videos: [
         {
           title: 'Original Video',
-          _id: 'og',
+          _id: 0,
           duration: [0, 52],
           url: 'http://grochtdreis.de/fuer-jsfiddle/video/sintel_trailer-480.mp4',
           controls: true,
           removable: false
-        },
-        {
-          title: 'Clip 1',
-          _id: '6-20',
-          duration: [6, 20],
-          url: 'http://grochtdreis.de/fuer-jsfiddle/video/sintel_trailer-480.mp4#t=6,20',
-          controls: false,
-          removable: true
-        },
-        {
-          title: 'Clip 2',
-          _id: '24-30',
-          duration: [24, 30],
-          url: 'http://grochtdreis.de/fuer-jsfiddle/video/sintel_trailer-480.mp4#t=24,30',
-          controls: false,
-          removable: true
-        },
-        {
-          title: 'Clip 3',
-          _id: '30-36',
-          duration: [30, 36],
-          url: 'http://grochtdreis.de/fuer-jsfiddle/video/sintel_trailer-480.mp4#t=30,36',
-          controls: false,
-          removable: true
-        },
+        }
       ],
       selectedVideo:         {
         title: 'Original Video',
-        _id: 'og',
+        _id: 1,
         duration: [0, 52],
         url: 'http://grochtdreis.de/fuer-jsfiddle/video/sintel_trailer-480.mp4',
         controls: true,
@@ -57,29 +33,38 @@ class App extends Component{
     const onVideoSelect =  selectedVideo => {
       console.log(selectedVideo)
       return this.setState({selectedVideo})
-    }
+    };
+
     const onVideoEdit =  selectedVideo => {
       console.log(selectedVideo)
       // return this.setState({selectedVideo})
-    }
+    };
+
     const onVideoRemove =  selectedVideo => {
 
       return this.setState({videos: this.state.videos.filter((video)=>{return video._id !== selectedVideo }) })
-    }
+    };
+
     const onUpdateClipping =  selectedVideo => {
-      this.setState({selectedVideo: {
+      const UpdatedVideo = {
         title: selectedVideo.title,
-        _id: `${selectedVideo.start}-${selectedVideo.end}`,
+        _id: selectedVideo._id,
         duration: [selectedVideo.start, selectedVideo.end],
         url: `http://grochtdreis.de/fuer-jsfiddle/video/sintel_trailer-480.mp4#t=${selectedVideo.start},${selectedVideo.end}`,
         controls: false,
         removable: true
-      }})
 
-      return this.setState({videos: this.state.videos.filter((video)=>{return video._id !== selectedVideo }) })
-    }
+    };
+    let newVideos = [...this.state.videos];
+    newVideos.splice(selectedVideo._id, 1, UpdatedVideo);
+      this.setState({selectedVideo: UpdatedVideo})
+//       console.log(selectedVideo._id, ...this.state.videos, UpdatedVideo);
+// console.log(.splice(2, 1, UpdatedVideo))
+      this.setState({videos: newVideos})
+    };
+
     const onCreateClipping =  selectedVideo => {
-this.setState({videos: [...this.state.videos, selectedVideo]})
+      this.setState({videos: [...this.state.videos, selectedVideo]})
 
       // return this.setState({videos: this.state.videos.filter((video)=>{return video._id !== selectedVideo }) })
     }
@@ -88,7 +73,7 @@ this.setState({videos: [...this.state.videos, selectedVideo]})
         <div className="row border" >
           <div className="no-padding col-lg-8 col-xs-12">
             <VideoPlayer video={this.state.selectedVideo}/>
-            <VideoClipForm onUpdateClipping={onUpdateClipping} onCreateClipping={onCreateClipping} removable={this.state.selectedVideo.removable} duration={this.state.selectedVideo.duration} title={this.state.selectedVideo.title}/>
+            <VideoClipForm onUpdateClipping={onUpdateClipping} onCreateClipping={onCreateClipping} _id={this.state.selectedVideo._id} removable={this.state.selectedVideo.removable} duration={this.state.selectedVideo.duration} title={this.state.selectedVideo.title}/>
           </div>
 
           <VideoList
